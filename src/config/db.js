188374e3ff6ce0cc45dc6bc43444b980e62783f4
Diego França <diego.franca92@gmail.com>
2019-05-20 16:53:21 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
   // Your web app's Firebase configuration
   let firebaseConfig = {
-    apiKey: "AIzaSyDcz0l_dzWJNy38F-n9JoiP2F7SGjeoAks",
-    authDomain: "crud-vue-fe38e.firebaseapp.com",
-    databaseURL: "https://crud-vue-fe38e.firebaseio.com",
-    projectId: "crud-vue-fe38e",
-    storageBucket: "crud-vue-fe38e.appspot.com",
-    messagingSenderId: "130293861528",
-    appId: "1:130293861528:web:1370b6be77ef3c35"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   let app = firebase.initializeApp(firebaseConfig)
